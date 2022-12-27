fd220cc01c9a9d6af5cc6ba64e3a2e142d04f436
Seven <seven@diqigan.cn>
2022-12-28 00:56:26 +0800

    添加二级菜单图标

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,21 @@
-document.querySelectorAll('.nav>li>a').forEach(item => {
-  item.addEventListener('click', (event) => {
-    let elementA = event.target;
-    let subNav = elementA.parentNode.querySelector('.nav');
-    let subNavStyleDisplay = subNav.style.display
-    subNav.style.display = subNavStyleDisplay === 'flex' ? 'none' : 'flex';
-  })
+// document.querySelectorAll('.nav>li>a').forEach(item => {
+//   item.addEventListener('click', (event) => {
+//     let elementA = event.target;
+//     let subNav = elementA.parentNode.querySelector('.nav');
+//     let subNavStyleDisplay = subNav.style.display
+//     subNav.style.display = subNavStyleDisplay === 'flex' ? 'none' : 'flex';
+//   })
 
-  subNav = item.parentNode.querySelector('.nav')
-  if (subNav) {
-    let elementSpanIcon = subNav.previousElementSibling;
-    if (elementSpanIcon) {
-      elementSpanIcon.style.display = 'inline-block';
-    }
-  }
+//   subNav = item.parentNode.querySelector('.nav')
+//   if (subNav) {
+//     let elementSpanIcon = subNav.previousElementSibling;
+//     if (elementSpanIcon) {
+//       elementSpanIcon.style.display = 'inline-block';
+//     }
+//   }
+// })
+
+document.querySelectorAll('.nav>li>.nav').forEach(item => {
+  let elementIcon = item.parentNode.querySelector('span.iconfont');
+  elementIcon.style.display = 'inline';
 })
